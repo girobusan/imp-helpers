94a7724bc8b9142d42d7d0b6e8efa49787e28540
Gleb TZ <gleb@>
2024-04-19 22:53:27 +0300

    test build

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const webpack = require('webpack');
+const pkg = require('./package.json');
+
+const env = process.env.NODE_ENV;
+
+module.exports = function (env, argv) {
+
+  let builddir = argv.mode== 'production' ? 'helpers' : 'test/helpers';
+
+  let baseConf =  {
+    watch: argv.mode != 'production',
+    target: 'web',
+    optimization: {
+    },
+    mode: argv.mode,
+    entry: {
+      "vega-embed": './src/vega-embed.js',
+    },
+    devtool: argv.mode != "production" ? 'inline-source-map' : false, 
+    devServer: argv.mode != "production" ? {contentBase: 'docs'} : {contentBase: 'test'},
+    output: {
+      path: path.resolve(__dirname, builddir, "")
+    },
+    module: {
+      rules: [
+        {
+          test: /\.svg$/,
+          resourceQuery: /raw/,
+          type: 'asset/source'
+        },
+
+        {
+          test: /\.svg$/,
+          resourceQuery: { not: [/raw/] },
+          type: 'asset/inline'
+        },
+
+        {
+          test: /\.(less|css|scss)$/,
+          use: [
+            'style-loader' ,
+            'css-loader',
+            'sass-loader'
+          ],
+        },
+        {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+      ]
+
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        // Definitions...
+        'VERSION': JSON.stringify(pkg.version)
+      }),
+    ],
+  };
+
+  // let libConf = Object.assign({} , baseConf);
+
+ 
+
+  return  baseConf  ;
+}
