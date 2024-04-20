df8345685670309deeb18395cde725b6168e50be
Gleb TZ <gleb@>
2024-04-20 18:09:19 +0300

    +vega-embed

--- src/vega-embed.js
+++ src/vega-embed.js
@@ -1,2 +1,64 @@
 import embed from 'vega-embed';
 
+const schemaString = "https://vega.github.io/schema/vega-lite/v4.json" ;
+
+function tracePath(obj , keys){
+  let cursor = obj;
+  keys.forEach( k=>{  
+    if(!cursor[k]){ cursor[k] = {} } 
+    cursor = cursor[k];
+    return;
+  })
+  return cursor;
+}
+
+function preRender(params , params_raw , subname){
+
+   const el = document.createElement("div");
+   el.style.width="600px";
+   const static_params = Object.assign({} , params);
+   tracePath(static_params, ["usermeta" , "embedOptions" , "renderer"]);
+   static_params.usermeta.embedOptions.renderer = "svg"
+   static_params.usermeta.embedOptions.actions = false;
+   static_params.width=600;
+
+   return embed(el , static_params , {"renderer": "svg"}  )
+   .then( ()=>el.outerHTML )
+}
+
+function render( params , params_raw , subname  ){
+  return `<div data-ihelper="vega-embed" >
+     <div class="vega_embed_view" style="width:100%">
+      ${ "Interactive viz" }
+     </div>
+    <script type="application/json">${params_raw}</script>
+  </div>`
+}
+
+function preview( params , params_raw , subname ){
+  return preRender(params,params_raw,subname) 
+}
+
+function animate(el){
+  var params ;
+  var embedOpts = {};
+  const element = el.querySelector(".vega_embed_view");
+  if(!element){ console.error("malformed html"); return }
+  try{
+    params = JSON.parse( el.querySelector("script").textContent );
+  }catch(e){
+     el.innerHTML = "Can not parse params!"
+     console.error("Can not parse params" , e)
+     return;
+  }
+  if(!("$schema" in params) ){ params.$schema = schemaString }
+  // element.innerHTML = "All good"
+  embed( element , params , embedOpts)
+}
+
+globalThis.impHelpers && globalThis.impHelpers.register( 
+   "vega-embed" , 
+   { animate , render , preview},
+   "json"
+   )
+
