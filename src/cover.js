d71a9379296e12849f2b19ef1259522768851fa3
Gleb TZ <gleb@>
2024-04-27 22:55:54 +0300

    cover improved

--- src/cover.js
+++ src/cover.js
@@ -1,20 +1,28 @@
+var API;
 
 function render( params , params_raw , subname ){
-console.log("render cover!" , params);
+    
    return ` <!--push content down-->
-   <div style="height:50vh; padding: 0 ; margin:0"></div>
+   <div style="height:${ params.heigt || '50vh' }; 
+   padding:0 ; 
+   margin:0 ; 
+   min-height: 300px;
+   margin-bottom:${params.marginBottom || '1rem'}"></div>
    <div 
    class="coverHelper"
    data-ihelper="cover"
    data-cleanup="${params.removeH1 || false}"
    style="position:absolute;
+   box-sizing: border-box;
+   padding: 1rem;
    top: 0;
    left:0;
    right:0;
-   height:50vh;
+   min-height: 300px;
+   height:${ params.height || '50vh'};
    background-color: ${params.bgColor || "black"};
    background-image: url(${params.bgImage||""});
-   color: ${params.color || "white"};
+   color: ${params.color || 'white'};
    background-size: cover;
    background-attachment: fixed;
    display: flex;
@@ -23,22 +31,20 @@ console.log("render cover!" , params);
    text-align: center;
    align-content: center;
    margin: 0;
-   padding:0;
    ">
-     <h1 class="coverHelperTitle" style="color: ${ params.color || "white" };
+     <h1 class="coverHelperTitle" style="color: ${ params.color || 'white' };
      margin: 0;
      position: sticky;
      ">
-     ${params.title || "Title"}</h1>
+     ${params.title || 'Title'}</h1>
      ${ params.subtitle ? "<div class='coverHelperSubtitle' style='font-size:1.2em'>"+params.subtitle+"</div>" : "" }
    </div>`
 }
 
 function preview( params, params_raw , subname ){
   if(params.noPreview){ 
-      return globalThis.impHelpers.defaultPreview("cover, preview render disabled" , `${params.title || "no title"}<br>------<br>${params.subtitle || "no subtitle"}`)
+      return API.defaultPreview("cover, preview render disabled" , `${params.title || "no title"}<br>------<br>${params.subtitle || "no subtitle"}`)
       }
-  // return globalThis.impHelpers.defaultPreview( "cober" , "This helper engages in view mode only" )
   return render(params, params_raw, subname);
 }
 
@@ -47,12 +53,15 @@ function animate(el){
      const headers = document.querySelectorAll("h1");
      let h1 = Array.from(headers).filter(e=>!e.classList.contains("coverHelperTitle"))[0];
      h1 && h1.remove();
-
   }
 }
 
-globalThis.impHelpers && globalThis.impHelpers.register(
+function init(api){
+  API = api;
+}
+
+globalThis.impHelpers.register(
    "cover",
-   { render, preview , animate },
+   { render, preview , animate , init },
    "yaml"
 )
