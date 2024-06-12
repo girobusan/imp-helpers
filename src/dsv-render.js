7a5730d9b62ebb9896a788d05dfa0e22a47e8fea
Gleb TZ <gleb@>
2024-06-12 18:42:30 +0300

    test beautify

--- src/dsv-render.js
+++ src/dsv-render.js
@@ -1,61 +1,72 @@
 import Papa from 'papaparse';
 const API = globalThis.impHelpers;
 const nameRx = /^@name:\s*(.+)\s*$/gm;
-const hideRx= /^@hide\s*$/gm;
+const hideRx = /^@hide\s*$/gm;
 const jsonRx = /\.json$/i;
 
-function row2html(r , tag){
-  if(!tag){ tag = "td" }
+function row2html(r, tag) {
+  const
+  if (!tag) {
+    tag = "td"
+  }
   let result = "<tr>";
-  r.forEach(d=>result += (`<${tag}> ${d} </${ tag }>`)) 
+  r.forEach(d => result += (`<${tag}> ${d} </${ tag }>`))
   return result + "</tr>"
 }
 
-function doRender(str , noHeader){
-   const parsed = Papa.parse(str.trim() , 
-   // {escapeChar:"\\"}
-   );
-   const data = parsed.data;
-   let html="<table>\n"
-   if(data.errors){ console.error(data.errors) }
-   if(!noHeader){
-      html+="<thead>\n"
-      html+=`${ row2html(data.shift() , "th") }\n`
-      html+="</thead>\n<tbody>\n"
-   }else{ html+="<tbody>" }
-   data.forEach( d=>html+=`${ row2html(d) }\n` );
-   return html + "</tbody></table>"
+function doRender(str, noHeader) {
+  const parsed = Papa.parse(str.trim(),
+    // escapeChar:"\\"
+  );
+  const data = parsed.data;
+  let html = "<table>\n"
+  if (data.errors) {
+    console.error(data.errors)
+  }
+  if (!noHeader) {
+    html += "<thead>\n"
+    html += `${ row2html(data.shift() , "th") }\n`
+    html += "</thead>\n<tbody>\n"
+  } else {
+    html += "<tbody>"
+  }
+  data.forEach(d => html += `${ row2html(d) }\n`);
+  return html + "</tbody></table>"
 }
 
-function render(params , params_raw , subname){
-  const save = nameRx.exec(params) ; //params.match(nameRx);
-  const hide = hideRx.exec(params) ; 
+function render(params, params_raw, subname) {
+  const save = nameRx.exec(params); //params.match(nameRx);
+  const hide = hideRx.exec(params);
   let strData = params;
-  if(hide){
-     strData= strData.replace(hideRx , "");
+  if (hide) {
+    strData = strData.replace(hideRx, "");
   }
-  if(save){ 
+  if (save) {
     let parse = save[1].toLowerCase().endsWith(".json")
 
-    strData = strData.replace(nameRx , "") ;
+    strData = strData.replace(nameRx, "");
     let saveData = strData.trim();
-    saveData =  parse ?  
-    Papa.parse( strData.trim() , 
-    { header: !( subname && subname.toLowerCase()=='no-header' )}).data : 
-    saveData;
-    
+    saveData = parse ?
+      Papa.parse(strData.trim(), {
+        header: !(subname && subname.toLowerCase() == 'no-header')
+      }).data :
+      saveData;
 
-    window.impData[save[1]] = { 
+
+    window.impData[save[1]] = {
       type: parse ? "object" : "string",
       data: saveData
     }
   }
-  const noHead = subname && subname.toLowerCase().trim()==='no-header';
-  return hide ? "" : doRender(strData , noHead);
+  const noHead = subname && subname.toLowerCase().trim() === 'no-header';
+  return hide ? "" : doRender(strData, noHead);
 }
 
-function preview(params , params_raw , subname){
-  return render(params , params_raw , subname);
+function preview(params, params_raw, subname) {
+  return render(params, params_raw, subname);
 }
 
-API.register( "dsv-render" , { render , preview } , "raw" );
+API.register("dsv-render", {
+  render,
+  preview
+}, "raw");
