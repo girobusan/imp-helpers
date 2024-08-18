836810ec888f37ec7aa02fe70c7a955c54d9250a
Girobusan <private>
2024-08-18 17:10:02 +0300

    +QR code

---
+++ src/qr-code.js
@@ -0,0 +1,42 @@
+const QRCode = require("qrcode");
+const API = globalThis.impHelpers;
+
+function makeCode(str, size, bg, fg, margin) {
+  // size = size || 256;
+  const opts = {
+    type: "svg",
+    margin: margin == undefined ? 4 : margin,
+    color: {
+      dark: fg || "#000000",
+      light: bg || "#ffffff",
+    },
+  };
+  console.log(opts);
+  if (size) {
+    opts.width = size;
+  }
+  return QRCode.toString(str, opts);
+}
+function render(params) {
+  if (!params.text) {
+    return "";
+  }
+  return makeCode(
+    params.text,
+    params.size,
+    params.bgColor,
+    params.fgColor,
+    params.margin,
+  )
+    .then((url) => `<div class='qr_code_container'>${url}</div>`)
+    .catch((e) => console.error(e));
+}
+
+function preview(params) {
+  if (!params || !params.text) {
+    return API.errorNotice("Can not render QR", "'text' parameter missing");
+  }
+  return render(params);
+}
+
+API.register("qr-code", { render, preview }, "yaml");
--- webpack.config.js
+++ webpack.config.js
@@ -1,83 +1,82 @@
-const path = require('path');
-const webpack = require('webpack');
-const pkg = require('./package.json');
-const TerserPlugin = require('terser-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const pkg = require("./package.json");
+const TerserPlugin = require("terser-webpack-plugin");
 
 // const env = process.env.NODE_ENV;
 
-module.exports = function (env, argv) {
+module.exports = function(env, argv) {
+  let builddir = argv.mode == "production" ? "helpers" : "test/helpers";
 
-  let builddir = argv.mode== 'production' ? 'helpers' : 'test/helpers';
-
-  let baseConf =  {
-    watch: argv.mode != 'production',
-    target: 'web',
+  let baseConf = {
+    watch: argv.mode != "production",
+    target: "web",
     optimization: {
-      minimizer: [new TerserPlugin({
-      extractComments: false,
-      terserOptions: {
-        format: {
-          comments: false,
-        },
-    }})]
+      minimizer: [
+        new TerserPlugin({
+          extractComments: false,
+          terserOptions: {
+            format: {
+              comments: false,
+            },
+          },
+        }),
+      ],
     },
     mode: argv.mode,
     entry: {
-      "vega-embed": './src/vega-embed.js',
-      "cover": './src/cover.js',
-      "dsv-render": './src/dsv-render.js',
-      "headers-nav": './src/headers-nav.js',
-      "placeholders": './src/placeholders.js',
-      "data-saver": './src/data-saver',
+      "vega-embed": "./src/vega-embed.js",
+      cover: "./src/cover.js",
+      "dsv-render": "./src/dsv-render.js",
+      "headers-nav": "./src/headers-nav.js",
+      placeholders: "./src/placeholders.js",
+      "data-saver": "./src/data-saver",
+      "qr-code": "./src/qr-code.js",
     },
-    devtool: argv.mode != "production" ? 'inline-source-map' : false, 
-    devServer: argv.mode != "production" ? {contentBase: 'docs'} : {contentBase: 'test'},
+    devtool: argv.mode != "production" ? "inline-source-map" : false,
+    devServer:
+      argv.mode != "production"
+        ? { contentBase: "docs" }
+        : { contentBase: "test" },
     output: {
-      path: path.resolve(__dirname, builddir, "")
+      path: path.resolve(__dirname, builddir, ""),
     },
     module: {
       rules: [
         {
           test: /\.svg$/,
           resourceQuery: /raw/,
-          type: 'asset/source'
+          type: "asset/source",
         },
 
         {
           test: /\.svg$/,
           resourceQuery: { not: [/raw/] },
-          type: 'asset/inline'
+          type: "asset/inline",
         },
 
         {
           test: /\.(less|css|scss)$/,
-          use: [
-            'style-loader' ,
-            'css-loader',
-            'sass-loader'
-          ],
+          use: ["style-loader", "css-loader", "sass-loader"],
         },
         {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: "babel-loader"
-        }
-      }
-      ]
-
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: "babel-loader",
+          },
+        },
+      ],
     },
     plugins: [
       new webpack.DefinePlugin({
         // Definitions...
-        'VERSION': JSON.stringify(pkg.version)
+        VERSION: JSON.stringify(pkg.version),
       }),
     ],
   };
 
   // let libConf = Object.assign({} , baseConf);
 
- 
-
-  return  baseConf  ;
-}
+  return baseConf;
+};
