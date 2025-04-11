05772d1bc5f54de210acbd1a023c612ffe330f28
Girobusan <private>
2025-04-11 20:52:36 +0300

    make sidebar static-friendly

--- src/sidebar/sidebar.js
+++ src/sidebar/sidebar.js
@@ -2,13 +2,28 @@ const MYNAME = "sidebar";
 let API;
 let viewMode = false;
 let resizer;
+let params;
 const csscode = require("./sidebar.css?raw");
 const svgbtn = require("../../src/icons/arrow_menu_open_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg?raw");
+
+function postprocess(html, _) {
+  const code = `<!--sidebar-->
+<style>
+${csscode}
+</style>
+<div id="IMPsidebar"  data-ihelper="${MYNAME}">
+ <iframe src="${(params.url || "menu.html") + "?mode=view"}" 
+style="position:absolute; top:0; left:0; width:100%;height:100%;border:none">
+  </iframe>
+<button>${svgbtn}</button>
+</div>
+`;
+  return code + html;
+}
 
 function addSideBar(url) {
   const old = document.querySelector("div#IMPsidebar");
   const main = document.querySelector("main#pageMain");
-  const footer = document.querySelector("footer#pageFooter");
   const style = document.querySelector("style#IMPsidebarCSS");
   //
   const sb = document.createElement("iframe");
@@ -71,19 +86,27 @@ function init(api, viewM) {
   viewMode = viewM;
 }
 
-function preview(params, _, subname) {
+function preview(para, _, subname) {
+  params = para;
   return API.defaultPreview(
     MYNAME,
     subname === "this" ? "This page is the sidebar" : "Sidebar added",
   );
 }
 
-function render(params, _, subname) {
-  return `<!--sidebar--><span data-ihelper="${MYNAME}" data-subname="${subname}" data-url="${params.url || "menu.html"}" hidden></span>`;
+function render(para, _, subname) {
+  params = para;
+  return ""; // `<!--sidebar--><span data-ihelper="${MYNAME}" data-subname="${subname}" data-url="${para.url || "menu.html"}" hidden></span>`;
 }
 
 function animate(el, params) {
   console.log("Animate sidebar");
+  if (el) {
+    el.classList.add("js-enabled");
+    const btn = el.querySelector("button");
+    btn.addEventListener("click", () => el.classList.toggle("expanded"));
+  }
+  return;
   if (!el && params) {
     //autoloaded
     addSideBar(params.url);
@@ -103,4 +126,5 @@ globalThis.impHelpers.register(
   MYNAME,
   { init, render, preview, autoload, animate },
   "yaml",
+  postprocess,
 );
