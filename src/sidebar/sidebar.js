22eb079c072fc83d702b45293bed28a5ae7c17a6
Girobusan <private>
2025-04-11 21:09:03 +0300

    sidebar code cleanup

--- src/sidebar/sidebar.js
+++ src/sidebar/sidebar.js
@@ -1,7 +1,6 @@
 const MYNAME = "sidebar";
 let API;
 let viewMode = false;
-let resizer;
 let params;
 const csscode = require("./sidebar.css?raw");
 const svgbtn = require("../../src/icons/arrow_menu_open_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg?raw");
@@ -19,66 +18,6 @@ style="position:absolute; top:0; left:0; width:100%;height:100%;border:none">
 </div>
 `;
   return code + html;
-}
-
-function addSideBar(url) {
-  const old = document.querySelector("div#IMPsidebar");
-  const main = document.querySelector("main#pageMain");
-  const style = document.querySelector("style#IMPsidebarCSS");
-  //
-  const sb = document.createElement("iframe");
-  const container = document.createElement("div");
-  const mbutton = document.createElement("button");
-  mbutton.innerHTML = svgbtn;
-  mbutton.addEventListener("click", () =>
-    container.classList.toggle("expanded"),
-  );
-
-  // container.style.position = "relative";
-  container.id = "IMPsidebar";
-
-  container.appendChild(sb);
-  container.appendChild(mbutton);
-
-  style && style.remove();
-  const newstyle = document.createElement("style");
-  newstyle.innerHTML = csscode;
-  newstyle.id = "IMPsidebarCSS";
-  document.head.appendChild(newstyle);
-
-  sb.style.border = "none";
-  sb.style.display = "block";
-  sb.style.top = "0px";
-  sb.style.left = "0px";
-  sb.style.width = "100%";
-  sb.style.height = "100%";
-  sb.style.position = "absolute";
-  sb.setAttribute("src", url + "?mode=view");
-
-  // main.style.float = "left";
-  // main.style.maxWidth = "calc(100vw - 350px)";
-  if (resizer) {
-    window.removeEventListener("resize", resizer);
-  }
-  resizer = () =>
-    (container.style.minHeight =
-      main.getBoundingClientRect().height +
-      // footer.getBoundingClientRect().height +
-      "px");
-  window.addEventListener("resize", resizer);
-
-  old && old.remove();
-  // container.style.float = "left";
-  container.style.minHeight =
-    main.getBoundingClientRect().height +
-    // footer.getBoundingClientRect().height +
-    "px";
-  container.style.marginRight = "1rem";
-
-  const fc = document.body.firstChild;
-  fc
-    ? document.body.insertBefore(container, fc)
-    : document.body.appendChild(container);
 }
 
 function init(api, viewM) {
@@ -107,19 +46,6 @@ function animate(el, params) {
     btn.addEventListener("click", () => el.classList.toggle("expanded"));
   }
   return;
-  if (!el && params) {
-    //autoloaded
-    addSideBar(params.url);
-    return;
-  }
-  if (el.dataset.subname === "this") {
-    let b = document.querySelector("base") || document.createElement("base");
-    b.setAttribute("target", "_top");
-    document.head.appendChild(b);
-    document.querySelector("footer#pageFooter").remove();
-    return;
-  }
-  addSideBar(el.dataset.url);
 }
 
 globalThis.impHelpers.register(
