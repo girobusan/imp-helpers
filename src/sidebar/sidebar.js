60aeaea0b4ae3a2ee6f0f0845c907eb2ed509ef9
Girobusan <private>
2025-04-12 12:44:37 +0300

    +autoload, animate for sidebar

--- src/sidebar/sidebar.js
+++ src/sidebar/sidebar.js
@@ -1,12 +1,13 @@
 const MYNAME = "sidebar";
 let API;
-let viewMode = false;
 let params;
+let subname;
+let viewMode = false;
+
 const csscode = require("./sidebar.css?raw");
 const svgbtn = require("../../src/icons/arrow_menu_open_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg?raw");
 
-function postprocess(html, _) {
-  const code = `<!--sidebar-->
+const sidebarCode = (params) => `<!--sidebar-->
 <style>
 ${csscode}
 </style>
@@ -17,7 +18,12 @@ style="position:absolute; top:0; left:0; width:100%;height:100%;border:none">
 <button>${svgbtn}</button>
 </div>
 `;
-  return code + html;
+
+function postprocess(html, _) {
+  if (subname) {
+    return html;
+  }
+  return sidebarCode(params) + html;
 }
 
 function init(api, viewM) {
@@ -25,22 +31,64 @@ function init(api, viewM) {
   viewMode = viewM;
 }
 
-function preview(para, _, subname) {
+function preview(para, _, subname_in) {
   params = para;
+  subname = subname_in;
+
   return API.defaultPreview(
     MYNAME,
-    subname === "this" ? "This page is the sidebar" : "Sidebar added",
+    subname ? "This page is the sidebar" : "Sidebar added",
   );
 }
 
-function render(para, _, subname) {
+function render(para, _, subname_in) {
   params = para;
-  return ""; // `<!--sidebar--><span data-ihelper="${MYNAME}" data-subname="${subname}" data-url="${para.url || "menu.html"}" hidden></span>`;
+  subname = subname_in;
+  //animate must run somehow in the page, shown as sidebar
+  return subname
+    ? `<!--sidebar--><span data-ihelper="${MYNAME}" data-subname="${subname}" data-target="${params.target || ""}" data-url="${para.url || "menu.html"}" hidden></span>`
+    : "";
 }
 
-function animate(el, params) {
-  console.log("Animate sidebar");
+// still not tested!
+function autoload(params_in, viewMode, subname) {
+  // in view mode:
+  //  attach sidebar if it's not attached
+  //  respect ignore list!
+  params = params_in;
+  if (subname) {
+    return;
+  }
+  if (viewMode) {
+    const test = document.querySelector("div#IMPsidebar");
+    if (test) {
+      return;
+    }
+    const m = document.querySelector("main#pageMain");
+    if (m) {
+      m.innerHTML = sidebarCode(params) + m.innerHTML;
+      return;
+    }
+  }
+  //  in edit mode
+  //  ...do postprocess...
+}
+
+function animate(el) {
+  console.log("Animate sidebar", el);
+  if (el && el.dataset.subname) {
+    // the page is shown in sidebar
+    // set base target for all links to something appropriate
+    // console.log("fixing base...");
+    let b = document.querySelector("base") || document.createElement("base");
+    b.setAttribute("target", el.dataset.target || "_top");
+    // console.log(b);
+    document.head.appendChild(b);
+    return;
+  }
   if (el) {
+    //we are at the page with attached sidebar
+    //add some nice touches...
     el.classList.add("js-enabled");
     const btn = el.querySelector("button");
     btn.addEventListener("click", () => el.classList.toggle("expanded"));
